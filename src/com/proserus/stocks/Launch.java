7065e267678ea0f7c73365ef4d656d693689f3c0
alex.belisleturcot <alex.belisleturcot@localhost>
2011-06-20 21:41:46 -0400

    remove unused images

---
+++ src/com/proserus/stocks/Launch.java
@@ -0,0 +1,26 @@
+package com.proserus.stocks;
+
+import com.proserus.stocks.view.general.Window;
+
+public class Launch {
+
+	// Get exchange rate from web
+	// Set default Currency
+	// Show/Hide columns
+	// Full proof list of Symbols...
+	// Can add symbols from files, transaction and symbols
+	// All tables editable
+	// Combobox for buy,sell,div
+	// Combobox for currency
+	// Combobox for symbols
+	// Default columns hidden at startup
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Window s;
+		Window.getInstance();
+	}
+
+}
