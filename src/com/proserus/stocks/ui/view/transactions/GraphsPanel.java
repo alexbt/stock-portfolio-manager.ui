45276e5fcd6496fd34a412e0d63af55ca1cc7cf3
alex.belisleturcot <alex.belisleturcot@localhost>
2011-10-26 20:32:19 -0400

    graphs

---
+++ src/com/proserus/stocks/ui/view/transactions/GraphsPanel.java
@@ -0,0 +1,79 @@
+package com.proserus.stocks.ui.view.transactions;
+
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import com.proserus.stocks.bp.events.Event;
+import com.proserus.stocks.bp.events.EventBus;
+import com.proserus.stocks.bp.events.EventListener;
+import com.proserus.stocks.bp.events.SwingEvents;
+import com.proserus.stocks.ui.controller.PortfolioController;
+import com.proserus.stocks.ui.controller.ViewControllers;
+import com.proserus.stocks.ui.view.general.Tab;
+
+public class GraphsPanel extends JPanel implements EventListener,ChangeListener{
+	private boolean needToRecalculate = false;
+	private boolean graphTabVisible = false;
+	
+	private PortfolioController controller = ViewControllers.getController();
+	SectorGraph sector = new SectorGraph();
+	CurrencyGraph currencies = new CurrencyGraph();
+	LabelGraph label = new LabelGraph();
+	YearGraph years = new YearGraph();
+	
+	public GraphsPanel(){
+		GridLayout gl = new GridLayout(2,2);
+//		gl.setHgap(5);
+//		gl.setVgap(5);
+		setLayout(gl);
+		add(sector);
+		add(label);
+		add(currencies);
+		add(years);
+		EventBus.getInstance().add(this, SwingEvents.YEAR_ANALYSIS_UPDATED,SwingEvents.CURRENCY_ANALYSIS_UPDATED,SwingEvents.SECTOR_ANALYSIS_UPDATED,SwingEvents.TRANSACTION_UPDATED,SwingEvents.LABEL_ANALYSIS_UPDATED);
+	}
+
+
+	@Override
+    public void update(Event event, Object model) {
+		if(event.equals(SwingEvents.SECTOR_ANALYSIS_UPDATED)){
+			sector.updateData(SwingEvents.SECTOR_ANALYSIS_UPDATED.resolveModel(model));
+		}else if(event.equals(SwingEvents.LABEL_ANALYSIS_UPDATED)){
+			label.updateData(SwingEvents.LABEL_ANALYSIS_UPDATED.resolveModel(model));
+		}else if(event.equals(SwingEvents.CURRENCY_ANALYSIS_UPDATED)){
+			currencies.updateData(SwingEvents.CURRENCY_ANALYSIS_UPDATED.resolveModel(model));
+		}else if(event.equals(SwingEvents.YEAR_ANALYSIS_UPDATED)){
+			years.updateData(SwingEvents.YEAR_ANALYSIS_UPDATED.resolveModel(model));
+		}else if (SwingEvents.TRANSACTION_UPDATED.equals(event)){
+			if(graphTabVisible){
+				controller.calculateSectorAnalysis();
+				controller.calculateLabelAnalysis();
+				controller.calculateYearAnalysis();
+				needToRecalculate = false;
+			}else{
+				needToRecalculate = true;
+			}
+		}
+    }
+
+	@Override
+    public void stateChanged(ChangeEvent arg0) {
+		if(((Tab)arg0.getSource()).getSelectedComponent().equals(this)){
+			graphTabVisible = true;
+			if(needToRecalculate){
+				controller.calculateSectorAnalysis();
+				controller.calculateLabelAnalysis();
+				controller.calculateYearAnalysis();
+				needToRecalculate = false;
+			}
+		}else{
+			graphTabVisible = false;
+		}
+    }
+
+}
+
+
---
+++ src/com/proserus/stocks/ui/view/transactions/SectorGraph.java
@@ -0,0 +1,49 @@
+package com.proserus.stocks.ui.view.transactions;
+
+import java.text.NumberFormat;
+import java.util.Collection;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+
+import com.proserus.stocks.bo.analysis.SectorAnalysis;
+
+public class SectorGraph extends ChartPanel  {
+
+	public SectorGraph() {
+	    super(null);
+	    // TODO Auto-generated constructor stub
+    }
+	
+	public void updateData(Collection<? extends SectorAnalysis> col){
+		
+		JFreeChart chart =ChartFactory.createPieChart(
+	            "Market Value by Sectors (%)",  // chart title
+	            createDataset(col),             // data
+	            false,               // include legend
+	            false,
+	            false
+	        );
+		final PiePlot plot = (PiePlot) chart.getPlot();
+//		plot.set
+//		PieRenderer renderer = ((BarRenderer) chart.getPlot().getRenderer());
+		plot.setLabelGenerator(new StandardPieSectionLabelGenerator(
+	            "{0} = {2}", NumberFormat.getNumberInstance(), NumberFormat.getPercentInstance()
+	        ));
+		setChart(chart);
+	}
+	
+	private static PieDataset createDataset(Collection<? extends SectorAnalysis> col) {
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        for(SectorAnalysis analysis: col){
+        	dataset.setValue(analysis.getSector().getTitle(), analysis.getMarketValue().floatValue());
+        }
+        return dataset;        
+    }
+
+}
