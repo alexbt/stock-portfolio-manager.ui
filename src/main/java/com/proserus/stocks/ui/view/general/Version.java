cff759b6f47425205a405654d86a1182c928f9d1
alex belisleturcot <alex.belisleturcot@gmail.com>
2015-01-02 08:29:01 -0500

    Ignore historical prices in import test

--- src/main/java/com/proserus/stocks/ui/view/general/Version.java
+++ src/main/java/com/proserus/stocks/ui/view/general/Version.java
@@ -6,7 +6,7 @@
 							com.proserus.stocks.ui.view.general;
 
 							public interface Version {
-							public static String TIMESTAMP = "20140701-1012";
+							public static String TIMESTAMP = "20150101-1524";
 							public static String VERSION = "0.24";
 							public static
 							String VERSION_SUFFIX = "Beta";
