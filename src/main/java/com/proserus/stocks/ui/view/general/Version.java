313e726a3f7cdd316b63fc26ece64b81fae142eb
alex belisleturcot <alex.belisleturcot@gmail.com>
2015-01-02 13:42:18 -0500

    nicer format for the generated "Version.java"

--- src/main/java/com/proserus/stocks/ui/view/general/Version.java
+++ src/main/java/com/proserus/stocks/ui/view/general/Version.java
@@ -1,13 +1,11 @@
 /**
-							The content of this file is replaced every time the
-							java source files are compiled by maven
-							**/
-							package
-							com.proserus.stocks.ui.view.general;
+ * The content of this file is replaced every time the
+ * java source files are compiled by maven
+ **/
+package com.proserus.stocks.ui.view.general;
 
-							public interface Version {
-							public static String TIMESTAMP = "20150101-1524";
-							public static String VERSION = "0.24";
-							public static
-							String VERSION_SUFFIX = "Beta";
-							}
+public interface Version {
+    public static String TIMESTAMP = "20150102-1341";
+    public static String VERSION = "0.24";
+    public static String VERSION_SUFFIX = "Beta";
+}
